46da11466d71b4418646b5f3a918deca3cd82465
Robert Nelson <robertcnelson@gmail.com>
2020-02-03 13:33:57 -0600

    init
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ version.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+#
+ARCH=$(uname -m)
+
+config="multi_v7_defconfig"
+
+build_prefix="-stm32-x"
+branch_prefix="v"
+branch_postfix=".x"
+
+#arm
+KERNEL_ARCH=arm
+#toolchain="gcc_linaro_eabi_4_8"
+#toolchain="gcc_linaro_eabi_4_9"
+#toolchain="gcc_linaro_eabi_5"
+#toolchain="gcc_linaro_eabi_6"
+#toolchain="gcc_linaro_eabi_7"
+#toolchain="gcc_arm_eabi_8"
+#toolchain="gcc_arm_eabi_9"
+#toolchain="gcc_linaro_gnueabihf_4_7"
+#toolchain="gcc_linaro_gnueabihf_4_8"
+#toolchain="gcc_linaro_gnueabihf_4_9"
+#toolchain="gcc_linaro_gnueabihf_5"
+#toolchain="gcc_linaro_gnueabihf_6"
+#toolchain="gcc_linaro_gnueabihf_7"
+toolchain="gcc_arm_gnueabihf_8"
+#toolchain="gcc_arm_gnueabihf_9"
+#arm64
+#KERNEL_ARCH=arm64
+#toolchain="gcc_linaro_aarch64_gnu_5"
+#toolchain="gcc_linaro_aarch64_gnu_6"
+#toolchain="gcc_linaro_aarch64_gnu_7"
+#toolchain="gcc_arm_aarch64_gnu_8"
+#toolchain="gcc_arm_aarch64_gnu_9"
+
+#Kernel
+KERNEL_REL=4.19
+KERNEL_TAG=${KERNEL_REL}.100
+kernel_rt=".100-rt41"
+#Kernel Build
+BUILD=${build_prefix}46
+
+#v5.X-rcX + upto SHA
+#prev_KERNEL_SHA=""
+#KERNEL_SHA=""
+
+#git branch
+BRANCH="${branch_prefix}${KERNEL_REL}${branch_postfix}"
+
+DISTRO=xross
+DEBARCH=armhf
+#
